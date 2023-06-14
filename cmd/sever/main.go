129662eacef9a7624e00279956c9297781ba0b0e
tobytop <homelea@163.com>
2023-06-14 17:27:22 +0800

    fixed the example

--- cmd/sever/main.go
+++ cmd/sever/main.go
@@ -1,19 +1,15 @@
 package main
 
 import (
-	"fmt"
 	pb "go-rocket/example/proto/hello"
 	"go-rocket/mash"
 	"go-rocket/metadata"
 	"go-rocket/service"
 
 	"log"
-	"reflect"
 )
 
 func main() {
-	test := reflect.TypeOf(pb.HelloRequest{})
-	fmt.Println(test.Name())
 	mash := mash.NewMash()
 	mash.BuliderRouter(
 		service.BuilderBalance(service.None),
